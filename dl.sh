8d25dc496cfb814898e6837569a3a20cc06b36b3
Eitan Rousso <eitanr@wix.com>
2019-03-28 13:36:08 +0200

    remove the if file exists

--- dl.sh
+++ dl.sh
@@ -5,11 +5,4 @@ filename="$2"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 rm "cookie"
-bold=$(tput bold)
-if [[ ! -f "$2" ]]; then
-    echo "${bold}Downloading $2 failed :("
-else
-    echo "${bold}Finished downloading $2"
-fi
-
-rm "dl.sh"
+ls
