6b8e4fc6587f6d617c5089102a4119aac138581b
Eitan Rousso <eitanr@wix.com>
2019-03-28 13:09:45 +0200

    change to dl script

--- dl.sh
+++ dl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "arg0 is $0"
-echo "arg1 is $1"
-echo "arg2 is $2"
+fileid="$1"
+filename="$2"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
