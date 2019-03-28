bdf3a94181567df4b39b4dd523e60e534747b07d
Eitan Rousso <eitanr@wix.com>
2019-03-28 13:15:14 +0200

    remove cookie and print a finish message

--- dl.sh
+++ dl.sh
@@ -4,3 +4,5 @@ fileid="$1"
 filename="$2"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm cookie
+echo "Finished downloading $2"
