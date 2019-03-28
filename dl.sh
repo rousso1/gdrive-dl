bcfd30b21180bbcfc38df64afc45696e38db4755
Eitan Rousso <eitanr@wix.com>
2019-03-28 13:26:34 +0200

    print success/fail message

--- dl.sh
+++ dl.sh
@@ -5,4 +5,9 @@ filename="$2"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 rm "cookie"
-echo "Finished downloading $2"
+bold=$(tput bold)
+if [ ! -f {$2} ]; then
+    echo "${bold}Downloading $2 failed :("
+else
+    echo "${bold}Finished downloading $2"
+fi
