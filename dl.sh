8cf5c400fce47e41cb47b636c69e9d14a5189299
Eitan Rousso <eitanr@wix.com>
2019-03-28 13:30:05 +0200

    fix if

--- dl.sh
+++ dl.sh
@@ -6,7 +6,7 @@ curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 rm "cookie"
 bold=$(tput bold)
-if [ ! -f {$2} ]; then
+if [[ ! -f "$2" ]]; then
     echo "${bold}Downloading $2 failed :("
 else
     echo "${bold}Finished downloading $2"
